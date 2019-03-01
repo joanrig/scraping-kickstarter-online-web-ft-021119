946800dd07330a57d26965ab93c05bfd24568122
Joan Lyness <joanrigdon@gmail.com>
2019-03-01 04:26:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 # title = project.css("h2.bbcard_name strong a").text
 # image = project.css("div.project-thumbnail a img").attribute("src").value
 # description = project.css("p.bbcard_blurb").text
-# location = project.css("")
+# location = project.css("li a.data.location")
 # percent_funded = project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
