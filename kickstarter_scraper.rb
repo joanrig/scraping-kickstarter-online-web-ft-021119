db8c8d427ecc0588e5021a97c84c87ddd4a14264
Joan Lyness <joanrigdon@gmail.com>
2019-03-01 04:07:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,8 @@ require 'pry'
 require 'nokogiri'
 
 # projects: kickstarter.css("li.project.grid_4")
+#title = <h2 class="bbcard_name">
+
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
