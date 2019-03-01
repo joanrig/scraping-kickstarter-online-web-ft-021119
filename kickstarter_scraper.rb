f82bb870c1850aec3dbc6616517f0049ad96e6f5
Joan Lyness <joanrigdon@gmail.com>
2019-03-01 04:18:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,10 @@
 require 'pry'
 require 'nokogiri'
 
-#projects: kickstarter.css("li.project.grid_4")
-#title = project.css("h2.bbcard_name strong a").text
-#image = project.css("div.project-thumbnail a img").attribute("src").value
+# projects: kickstarter.css("li.project.grid_4")
+# title = project.css("h2.bbcard_name strong a").text
+# image = project.css("div.project-thumbnail a img").attribute("src").value
+# description = project.css("p.bbcard_blurb").text
 
 
 def create_project_hash
